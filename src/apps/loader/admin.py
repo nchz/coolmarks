17035e6d7912bae97f1e1c47e5aecd1b8c243bc9
Ignacio Gonzalez <8524584+nchz@users.noreply.github.com>
2022-11-27 11:11:52 -0300

    add Tag model.

--- src/apps/loader/admin.py
+++ src/apps/loader/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
 
-from apps.loader.models import Bookmark
+from apps.loader.models import Bookmark, Tag
 
 
 class BookmarkAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Bookmark._meta.get_fields()]
+    list_display = [field.name for field in Bookmark._meta.get_fields()[:-1]]
     readonly_fields = (
         "id",
         "owner",
         "dt",
+        "tags",
+    )
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "label",
+    )
+    readonly_fields = (
+        "id",
+        "bookmarks_string",
     )
 
 
 admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(Tag, TagAdmin)
--- src/apps/loader/models.py
+++ src/apps/loader/models.py
@@ -1,3 +1,4 @@
+import re
 from urllib.parse import urlparse
 
 import requests
@@ -10,48 +11,77 @@ from rest_framework import serializers
 MAX_LENGTH = 200
 
 
+class Tag(models.Model):
+    label = models.CharField(max_length=MAX_LENGTH)
+
+    def __str__(self):
+        return self.label
+
+    def bookmarks_string(self):
+        return "\n".join(str(b) for b in self.bookmark_set.all())
+
+
 class Bookmark(models.Model):
     owner = models.ForeignKey(
         User,
-        editable=False,
         on_delete=models.CASCADE,
         related_name="bookmarks",
+        editable=False,
     )
-    dt = models.DateTimeField(
-        auto_now_add=True,
-    )
+    dt = models.DateTimeField(auto_now_add=True)
     link = models.URLField()
     domain = models.CharField(
-        editable=False,
         max_length=MAX_LENGTH,
+        editable=False,
     )
     title = models.CharField(
-        editable=False,
         max_length=MAX_LENGTH,
+        editable=False,
+    )
+    tags = models.ManyToManyField(Tag, default=None)
+
+    class Meta:
+        ordering = ("-dt",)
+
+    def __str__(self):
+        return f"({self.owner.username}) {self.title}"
+
+
+class BookmarkSerializer(serializers.ModelSerializer):
+    # tags = TagSerializer(many=True, read_only=False)
+    tags_string = serializers.CharField(
+        max_length=MAX_LENGTH,
+        default="",
+        write_only=True,
     )
 
-
-# TODO HyperlinkedModelSerializer
-class BookmarkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bookmark
-        exclude = (
+        depth = 1
+        fields = (
             "id",
+            "link",
+            "tags_string",
+            # read_only_fields
             "owner",
+            "dt",
+            "domain",
+            "title",
+            "tags",
         )
 
     def create(self, validated_data):
         link = validated_data["link"]
 
         # process link to get required values.
-        domain = urlparse(link).netloc[:MAX_LENGTH]
-
-        r = requests.get(link)
-        tree = etree.fromstring(r.text, parser=etree.HTMLParser())
         try:
+            r = requests.get(link)
+            tree = etree.fromstring(r.text, parser=etree.HTMLParser())
             title = tree.xpath("//html/head/title")[0].text[:MAX_LENGTH]
-        except (IndexError, TypeError):
+        except (requests.exceptions.ConnectionError, IndexError, TypeError):
             title = "NO TITLE"
+
+        domain = urlparse(link).netloc[:MAX_LENGTH]
 
         # create and return object.
         bookmark = Bookmark(
@@ -61,4 +91,14 @@ class BookmarkSerializer(serializers.ModelSerializer):
             title=title,
         )
         bookmark.save()
+
+        if tags_string := validated_data["tags_string"]:
+            for tag in tags_string.split(";"):
+                # clean string and create tag (if any).
+                tag = re.sub(r"[\s|\-|_]+", "_", tag)
+                tag = re.sub(r"\W", "", tag)
+                tag = re.sub(r"_+", "_", tag).strip("_")
+                if tag:
+                    bookmark.tags.create(label=tag)
+
         return bookmark
