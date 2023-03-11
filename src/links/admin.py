81dd430774ff7fb2e814bd49c2343df6d845e815
Ignacio Gonzalez <nchngz@gmail.com>
2023-03-11 10:08:07 -0300

    add inlines in admin site.

--- src/links/admin.py
+++ src/links/admin.py
@@ -3,26 +3,34 @@ from django.contrib import admin
 from links.models import Link, Tag
 
 
+class LinkTagsInline(admin.TabularInline):
+    model = Link.tags.through
+
+
+@admin.register(Link)
 class LinkAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Link._meta.get_fields()[:-1]]
-    readonly_fields = (
+    readonly_fields = [
+        "dt",
         "id",
         "owner",
-        "dt",
         "tags",
-    )
+    ]
+    inlines = [
+        LinkTagsInline,
+    ]
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        super().save_model(request, obj, form, change)
 
 
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    list_display = (
+    list_display = [
         "id",
         "name",
-    )
-    readonly_fields = (
-        "id",
-        "links_string",
-    )
-
-
-admin.site.register(Link, LinkAdmin)
-admin.site.register(Tag, TagAdmin)
+    ]
+    inlines = [
+        LinkTagsInline,
+    ]
